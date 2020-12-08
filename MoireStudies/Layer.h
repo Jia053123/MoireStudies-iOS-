bafa28f175de393a47dcab14afa3c273615bc525
ErnestX <xiangspecial2012@gmail.com>
2020-12-08 01:52:52 -0500

    add Pattern class with stub

---
+++ MoireStudies/Layer.h
@@ -0,0 +1,23 @@
+//
+//  Layer.h
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-08.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Pattern : NSObject
+
+@property double speed;
+@property double direction;
+@property double fillRatio;
+@property double zoomRatio;
+
+- (Pattern*) initWithSpeed: (double) s Direction: (double) d FillRatio: (double) f ZoomRatio: (double) z;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MoireStudies/Pattern.m
@@ -0,0 +1,12 @@
+//
+//  Pattern.m
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-08.
+//
+
+#import "Pattern.h"
+
+@implementation Pattern
+
+@end
