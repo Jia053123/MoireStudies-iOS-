2bbbcfecaaa8adc89ae7d7180e698f0c65c9c408
ErnestX <xiangspecial2012@gmail.com>
2020-12-07 23:08:56 -0500

    Initial Commit

---
+++ MoireStudies/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-07.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MoireStudies/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-07.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
