9e17a95feb05610008d67505b6d165a5ee76b9aa
ErnestX <xiangspecial2012@gmail.com>
2020-12-17 19:37:03 -0500

    commit before converting to swift

--- MoireStudies/TileLayer.h
+++ MoireStudies/TileLayer.h
@@ -5,13 +5,16 @@
 //  Created by Jialiang Xiang on 2020-12-08.
 //
 
+#import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TileLayer : CALayer
 
-@property CALayer* fillLayer;
+@property double fillRatio;
+
+- (TileLayer*)initWithFillRatio: (double)f;
 
 @end
 
--- MoireStudies/TileLayer.m
+++ MoireStudies/TileLayer.m
@@ -5,8 +5,14 @@
 //  Created by Jialiang Xiang on 2020-12-08.
 //
 
+
 #import "TileLayer.h"
 
 @implementation TileLayer
+
+- (TileLayer*)initWithFillRatio: (double)f {
+    self.borderColor = [UIColor redColor].CGColor;
+    return self;
+}
 
 @end
