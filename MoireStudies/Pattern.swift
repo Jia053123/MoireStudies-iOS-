688110de4cd547b358fdc99133d056a9f06f6486
ErnestX <xiangspecial2012@gmail.com>
2020-12-23 02:26:01 -0500

    create Pattern in swift

---
+++ MoireStudies/Pattern.swift
@@ -0,0 +1,15 @@
+//
+//  Pattern.swift
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-23.
+//
+
+import Foundation
+
+struct Pattern {
+    var speed: Double
+    var direction: Double
+    var fillRatio: Double
+    var zoomRatio: Double
+}
