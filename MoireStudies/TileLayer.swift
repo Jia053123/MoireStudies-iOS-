49f183f7a40adcd242e650240ea7d6d8985f7fd9
ErnestX <xiangspecial2012@gmail.com>
2020-12-23 01:24:41 -0500

    create TileLayer in swift

---
+++ MoireStudies/TileLayer.swift
@@ -0,0 +1,14 @@
+//
+//  TileLayer.swift
+//  MoireStudies
+//
+//  Created by Jialiang Xiang on 2020-12-23.
+//
+
+import Foundation
+
+class TileLayer: CALayer {
+    func setUp(fillRatio: Double) {
+        self.borderColor = UIColor.red.cgColor
+    }
+}
