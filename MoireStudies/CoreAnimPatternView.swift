e547a054301285f72063f38acbe3cbadb5b6a4f0
ErnestX <xiangspecial2012@gmail.com>
2020-12-23 21:53:43 -0500

    change PatternView sizing logic; stubs

--- MoireStudies/CoreAnimPatternView.swift
+++ MoireStudies/CoreAnimPatternView.swift
@@ -10,26 +10,38 @@ import UIKit
 
 class CoreAnimPatternView: PatternView {
     private var tileHeight : Double = 10.0
-    private var tileLength : Double?
-    private var numOfTile : Int?
+    private var tileLength : Double = 0.0
+    private var numOfTile : Int = 0
+    private var tiles : Array<TileLayer> = Array()
     
     override func setUp() {
-        tileLength = Double(sqrt(pow(Float(self.frame.width), 2) + pow(Float(self.frame.height), 2)))
-        if let tileLength = tileLength {
-            numOfTile = Int(ceil(Double(tileLength) / tileHeight))
-            if let numOfTile = numOfTile {
-                for i in 1...numOfTile {
-                    let xPos : Double = Double(self.frame.width / 2)
-                    let yPos : Double = Double(i) * tileHeight
-                    
-                    let newTile = TileLayer()
-                    newTile.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-                    newTile.frame = CGRect(x: 0, y: 0, width: tileLength, height: tileHeight)
-                    newTile.position = CGPoint(x: xPos, y: yPos)
-                    newTile.setUp(fillRatio: 0.5)
-                    self.layer.addSublayer(newTile)
-                }
-            }
+        // the tiles are placed to fill the view
+        tileLength = Double(self.frame.width)
+        numOfTile = Int(ceil(Double(tileLength) / tileHeight)) + 1
+        
+        for i in 1...numOfTile {
+            let xPos : Double = Double(self.frame.width / 2)
+            let yPos : Double = Double(i) * tileHeight
+            
+            let newTile = TileLayer()
+            newTile.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            newTile.frame = CGRect(x: 0, y: 0, width: tileLength, height: tileHeight)
+            newTile.position = CGPoint(x: xPos, y: yPos)
+            newTile.setUp(fillRatio: 0.5) // TODO
+            self.layer.addSublayer(newTile)
+            tiles.append(newTile)
         }
+        self.animateTiles()
+    }
+    
+    func animateTiles() {
+        for t in tiles {
+            self.animateTile(tile: t)
+        }
+    }
+    
+    func animateTile(tile: TileLayer) {
+        // calculate distance till the end of the frame
+        // calculate duration from distance so that speed is fixed
     }
 }
--- MoireStudies/MoireView.swift
+++ MoireStudies/MoireView.swift
@@ -12,28 +12,30 @@ class MoireView: UIView {
     typealias PatternViewSubclass = CoreAnimPatternView
     typealias ControlViewSubclass = SliderControlView
     
+    var controlFrame1 = CGRect(x: 10, y: 30, width: 200, height: 300)
     var patternView1: PatternView?
     var controlView1: ControlView?
     var patternView2: PatternView?
     var controlView2: ControlView?
     
     @objc func setUp() {
-        let controlFrame1 = CGRect(x: 10, y: 30, width: 200, height: 300)
-        patternView1 = PatternViewSubclass.init(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
+        let diagonalLength = Double(sqrt(pow(Float(self.frame.width), 2) + pow(Float(self.frame.height), 2)))
+        patternView1 = PatternViewSubclass.init(frame: CGRect(x: 0, y: 0, width: diagonalLength, height: diagonalLength))
+        patternView1?.center = CGPoint(x: self.frame.width/2.0, y: self.frame.height/2.0)
         patternView1?.setUp()
         if let pv = patternView1 {
             self.addSubview(pv)
         }
         // TODO: setup patternView2
         //self.setUpMaskOnPatternView(patternView: patternView2, controlFrame: controlFrame1)
-        self.setUpControlsWithControlFrame(frame: controlFrame1)
+        self.setUpControls(frame: controlFrame1)
     }
     
     func setUpMaskOnPatternView(patternView: UIView, controlFrame: CGRect) {
         // TODO: mask corresponding pattern view to match the control views
     }
     
-    func setUpControlsWithControlFrame(frame: CGRect) {
+    func setUpControls(frame: CGRect) {
         controlView1 = ControlViewSubclass.init(frame: frame)
         if let cv = controlView1 {
             self.addSubview(cv)
