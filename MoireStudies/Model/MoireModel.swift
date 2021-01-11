9132143a74948dd11d9aa4c952194da03c5081b7
ErnestX <xiangspecial2012@gmail.com>
2021-01-11 16:58:03 -0500

    MoireModel stubs

--- MoireStudies/Model/MoireModel.swift
+++ MoireStudies/Model/MoireModel.swift
@@ -6,7 +6,11 @@
 //
 
 import Foundation
+import UIKit
 
+/**
+ Summary: represent the model of a single moire
+ */
 class MoireModel: Codable {
     private var _model: Array<Pattern> = []
     var model: Array<Pattern> {
@@ -20,9 +24,20 @@ class MoireModel: Codable {
             _model = newValue
         }
     }
+    private var _iconData: Data?
+    var icon: UIImage? {
+        get {
+            return nil
+        }
+        set {
+            
+        }
+    }
+    
     func reset() {
         self._model = []
         self._model.append(Pattern.demoPattern1())
         self._model.append(Pattern.demoPattern2())
+        // TODO: reset icon
     }
 }
