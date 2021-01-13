22497c56aebd2f0eeddba22e640f01c47b8ff265
ErnestX <xiangspecial2012@gmail.com>
2021-01-13 01:28:58 -0500

    unit tests stubs

---
+++ MoireStudiesTests/MoireModelTests.swift
@@ -0,0 +1,39 @@
+//
+//  SaveFileIOTests.swift
+//  MoireStudiesTests
+//
+//  Created by Jialiang Xiang on 2021-01-13.
+//
+
+import Foundation
+import XCTest
+
+@testable import MoireStudies
+
+class MoireModelTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testCreateMoires() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testSaveNewMoires() throws {
+        
+    }
+    
+    func testUpdateExistingMoires() throws {
+        
+    }
+    
+    func testDeleteMoires() throws {
+
+    }
+    
+    func testMultipleOperations() throws {
+        
+    }
+}
---
+++ MoireStudiesTests/SaveFileIOTests.swift
@@ -0,0 +1,14 @@
+//
+//  SaveFileIOTests.swift
+//  MoireStudiesTests
+//
+//  Created by Jialiang Xiang on 2021-01-13.
+//
+
+import Foundation
+import XCTest
+
+@testable import MoireStudies
+class MoireModelTests: XCTestCase {
+    
+}
