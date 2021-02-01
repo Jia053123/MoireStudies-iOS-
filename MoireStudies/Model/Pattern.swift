97aaaa78e47f9a0d464f59bd7b66bff7a60b0873
ErnestX <xiangspecial2012@gmail.com>
2021-02-01 14:20:14 -0500

    start refactoring moire respresentations to black and white width

--- MoireStudies/Model/Pattern.swift
+++ MoireStudies/Model/Pattern.swift
@@ -9,37 +9,36 @@ import Foundation
 import UIKit
 
 struct Pattern: Equatable, Codable {
-    var speed: CGFloat
-    var direction: CGFloat
-    var fillRatio: CGFloat
-    var scaleFactor: CGFloat
+    var speed: CGFloat // unit: point/s
+    var direction: CGFloat // unit: rad
+    var blackWidth: CGFloat // unit: point
+    var whiteWidth: CGFloat // unit: point
     
     static func debugPattern() -> Pattern {
-        return Pattern(speed: 10.0, direction: 0.0, fillRatio: 0.5, scaleFactor: 2.0)
+        return Pattern(speed: 10.0, direction: 0.0, blackWidth: 10, whiteWidth: 20)
     }
     
     static func defaultPattern() -> Pattern {
-        return Pattern(speed: 10.0, direction: 0.0, fillRatio: 0.5, scaleFactor: 1.0)
+        let r = Utilities.convertToBlackWidthAndWhiteWidth(fillRatio: 0.5, scaleFactor: 1.0)
+        return Pattern(speed: 10.0, direction: 0.0, blackWidth: r.blackWidth, whiteWidth: r.whiteWidth)
     }
     
     static func randomPattern() -> Pattern {
         return Pattern(speed: CGFloat.random(in: 10.0...40.0),
                        direction: CGFloat.random(in: 0...2*CGFloat.pi),
-                       fillRatio: CGFloat.random(in: 0.1...0.9),
-                       scaleFactor: CGFloat.random(in: 1.0...2.0))
+                       blackWidth: CGFloat.random(in: 5...15),
+                       whiteWidth: CGFloat.random(in: 5...15))
     }
     
     static func demoPattern1() -> Pattern {
-        return Pattern(speed: 30.0,
-                       direction: CGFloat.pi/4,
-                       fillRatio: CGFloat.random(in: 0.3...0.7),
-                       scaleFactor: CGFloat.random(in: 1.6...2.9))
+        let r = Utilities.convertToBlackWidthAndWhiteWidth(fillRatio: CGFloat.random(in: 0.3...0.7),
+                                                           scaleFactor: CGFloat.random(in: 1.6...2.9))
+        return Pattern(speed: 30.0, direction: CGFloat.pi/4, blackWidth: r.blackWidth, whiteWidth: r.whiteWidth)
     }
     
     static func demoPattern2() -> Pattern {
-        return Pattern(speed: 30.0,
-                       direction: CGFloat.pi/4.0 + 0.025,
-                       fillRatio: CGFloat.random(in: 0.3...0.7),
-                       scaleFactor: CGFloat.random(in: 1.3...2.6))
+        let r = Utilities.convertToBlackWidthAndWhiteWidth(fillRatio: CGFloat.random(in: 0.3...0.7),
+                                                           scaleFactor: CGFloat.random(in: 1.3...2.6))
+        return Pattern(speed: 30.0, direction: CGFloat.pi/4.0 + 0.025, blackWidth: r.blackWidth, whiteWidth: r.whiteWidth)
     }
 }
