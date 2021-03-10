bec4363e55abdaaa05f24e2c012cbe704868fef0
ErnestX <xiangspecial2012@gmail.com>
2021-03-10 02:21:40 -0500

    refactor MockControlsViewController to expose the matcher it receieves

--- MoireStudiesTests/Mock Classes/MockControlsViewController.swift
+++ MoireStudiesTests/Mock Classes/MockControlsViewController.swift
@@ -13,22 +13,26 @@ class MockControlsViewController: ControlsViewController {
     private(set) var initPatterns: Array<Pattern>?
     private(set) var setUpPerformed: Bool = false
     private(set) var resetPerformed: Bool = false
+    private(set) var controlAndPatternMatcher: CtrlAndPatternMatcher?
     
     func resetTestingRecords() {
         self.currentPatterns = nil
         self.setUpPerformed = false
         self.resetPerformed = false
+        self.controlAndPatternMatcher = nil
     }
     
     override func setUp(patterns: Array<Pattern>, settings: InitSettings, matcher: CtrlAndPatternMatcher, delegate: PatternManager) {
         super.setUp(patterns: patterns, settings: settings, matcher: matcher, delegate: delegate)
         self.setUpPerformed = true
         self.initPatterns = patterns
+        self.controlAndPatternMatcher = matcher
     }
     
     override func reset(patterns: Array<Pattern>, settings: InitSettings, matcher: CtrlAndPatternMatcher, delegate: PatternManager) {
         super.reset(patterns: patterns, settings: settings, matcher: matcher, delegate: delegate)
         self.resetPerformed = true
         self.initPatterns = patterns
+        self.controlAndPatternMatcher = matcher
     }
 }
