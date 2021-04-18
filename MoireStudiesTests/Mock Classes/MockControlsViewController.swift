52148eca080a99114340d255e3c8836f6f923102
ErnestX <xiangspecial2012@gmail.com>
2021-04-18 17:05:04 -0400

    update MockControlsViewController

--- MoireStudiesTests/Mock Classes/MockControlsViewController.swift
+++ MoireStudiesTests/Mock Classes/MockControlsViewController.swift
@@ -14,6 +14,7 @@ class MockControlsViewController: ControlsViewController {
     private(set) var setUpPerformed: Bool = false
     private(set) var resetPerformed: Bool = false
     private(set) var ids: Array<String>?
+    private(set) var highDegIds: Array<String>?
     private(set) var configs: Configurations?
     
     func resetTestingRecords() {
@@ -24,18 +25,18 @@ class MockControlsViewController: ControlsViewController {
     }
     
     override func setUp(patterns: Array<Pattern>, configs: Configurations, ids: Array<String>, highDegIds: Array<String>, delegate: PatternManager) {
-        // TODO: hdid
         self.setUpPerformed = true
         self.initPatterns = patterns
         self.ids = ids
+        self.highDegIds = highDegIds
         self.configs = configs
     }
     
     override func reset(patterns: Array<Pattern>, configs: Configurations, ids: Array<String>, highDegIds: Array<String>, delegate: PatternManager) {
-        // TODO: hdid
         self.resetPerformed = true
         self.initPatterns = patterns
         self.ids = ids
+        self.highDegIds = highDegIds
         self.configs = configs
     }
 }
