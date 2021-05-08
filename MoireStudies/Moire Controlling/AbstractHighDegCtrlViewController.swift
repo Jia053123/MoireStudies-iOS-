1e73ce20355c26fbdc69fd2100621ba6f41f1f5b
ErnestX <xiangspecial2012@gmail.com>
2021-05-08 16:43:20 -0400

    add stubs

--- MoireStudies/Moire Controlling/AbstractHighDegCtrlViewController.swift
+++ MoireStudies/Moire Controlling/AbstractHighDegCtrlViewController.swift
@@ -12,6 +12,21 @@ protocol AbstractHighDegCtrlViewController: HighDegCtrlViewController {
     var id: String! { get set }
 }
 
+/// the index parameters are from the POV of this class. E.g. if this class controlls 3 patterns, then their indexes are respectively 0,1,2
 extension AbstractHighDegCtrlViewController {
+    func modifyPattern(index: Int, speed: CGFloat) -> Bool {
+        return false
+    }
     
+    func modifyPattern(index: Int, direction: CGFloat) -> Bool {
+        return false
+    }
+    
+    func modifyPattern(index: Int, blackWidth: CGFloat) -> Bool {
+        return false
+    }
+    
+    func modifyPattern(index: Int, whiteWidth: CGFloat) -> Bool {
+        return false
+    }
 }
