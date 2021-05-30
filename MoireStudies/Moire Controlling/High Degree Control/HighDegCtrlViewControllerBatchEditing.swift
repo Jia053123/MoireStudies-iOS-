8a8536c6b2c6480721d65010df12bf2b35d3e778
ErnestX <xiangspecial2012@gmail.com>
2021-05-30 13:25:43 -0400

    implement speed and direction adjustment

--- MoireStudies/Moire Controlling/High Degree Control/HighDegCtrlViewControllerBatchEditing.swift
+++ MoireStudies/Moire Controlling/High Degree Control/HighDegCtrlViewControllerBatchEditing.swift
@@ -14,6 +14,9 @@ class HighDegCtrlViewControllerBatchEditing: UIViewController, AbstractHighDegCt
     var patternsDelegate: PatternManager!
     let initPattern: Array<Pattern?>
     
+    private var previousNetSpeedMultiplier: CGFloat = 1.0
+    private var previousNetDirectionAdjustment: CGFloat = 0.0
+    
     required init(id: String, frame: CGRect, patterns: Array<Pattern?>) {
         self.id = id
         self.initPattern = patterns
@@ -28,6 +31,39 @@ class HighDegCtrlViewControllerBatchEditing: UIViewController, AbstractHighDegCt
     required init?(coder: NSCoder) {
         self.initPattern = []
         super.init(coder: coder)
+    }
+    
+    func adjustRelativeSpeed(netMultiplier: CGFloat) {
+        guard let currentPatterns = self.patternsDelegate.retrievePatterns(callerId: self.id) else {return}
+        let relativeMultiplier = netMultiplier - previousNetSpeedMultiplier
+        self.previousNetSpeedMultiplier = netMultiplier
+        for i in 0..<currentPatterns.count {
+            _ = self.modifyPattern(index: i, speed: currentPatterns[i].speed + currentPatterns[i].speed * relativeMultiplier)
+        }
+    }
+    
+    func adjustAllDirection(netAdjustment: CGFloat) {
+        // rotate the whole moire
+        guard let currentPatterns = self.patternsDelegate.retrievePatterns(callerId: self.id) else {return}
+        let relativeAdjustment = netAdjustment - previousNetDirectionAdjustment
+        self.previousNetDirectionAdjustment = netAdjustment
+        for i in 0..<currentPatterns.count {
+            _ = self.modifyPattern(index: i, direction: currentPatterns[i].direction + relativeAdjustment)
+        }
+    }
+    
+    func adjustAllBlackWidth(by increment: CGFloat) {
+        // increase and decrease the width
+        for i in 0..<self.initPattern.count {
+            _ = self.modifyPattern(index: i, blackWidth: increment)
+        }
+    }
+    
+    func adjustAllWhiteWidth(by increment: CGFloat) {
+        // increase and decrease the width
+        for i in 0..<self.initPattern.count {
+            _ = self.modifyPattern(index: i, whiteWidth: increment)
+        }
     }
 }
 
--- MoireStudies/Moire Controlling/High Degree Control/SliderHighDegreeCtrlView.swift
+++ MoireStudies/Moire Controlling/High Degree Control/SliderHighDegreeCtrlView.swift
@@ -31,18 +31,26 @@ class SliderHighDegreeCtrlView: UIView {
             self.addSubview(view)
             view.frame = self.bounds
         }
+        self.speedSlider.minimumValue = 0.5
+        self.speedSlider.maximumValue = 1.5
+        self.directionSlider.minimumValue = 0.0
+        self.directionSlider.maximumValue = Float.pi*2
     }
     
     @IBAction func speedChanged(_ sender: Any) {
+        self.target.adjustRelativeSpeed(netMultiplier: CGFloat(self.speedSlider.value))
     }
     
     @IBAction func directionChanged(_ sender: Any) {
+        self.target.adjustAllDirection(netAdjustment: CGFloat(self.directionSlider.value))
     }
     
     @IBAction func fillChanged(_ sender: Any) {
+        // TODO: stub
     }
     
     @IBAction func scaleChanged(_ sender: Any) {
+        // TODO: stub
     }
 }
 
